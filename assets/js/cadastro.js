92c887194df6840ecff863322de14f20ecf89b41
perkles <leoazevedo1450@gmail.com>
2022-11-26 23:29:43 -0300

    Atualização

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,80 +1,44 @@
-let nomeCadastro = document.querySelector("#nomeCadastro")
-let nomeLabel = document.querySelector("#nomeLabel")
+const cadastroForm = document.forms['cadastro'];
 
-let emailCadastro = document.querySelector("#emailCadastro")
-let emailLabel = document.querySelector("#emailLabel")
- 
-nomeCadastro.addEventListener("keyup", () => {
-    if(nomeCadastro.value.length <= 2) {
-        nomeLabel.setAttribute("style", "color: red")
-        nomeLabel.innerHTML = "Nome * Insira no minímo 3 caracteres"
-        nomeCadastro.setAttribute('style', 'border-color: red')
-    }else {
-        nomeLabel.setAttribute("style", "color: green")
-        nomeLabel.innerHTML = "Nome"
-        nomeCadastro.setAttribute('style', 'border-color: green')
-    }
-})
-
-emailCadastro.addEventListener("keyup", () => {
-    event.preventDefault()
-
-    if(
-        emailCadastro.value.length >= 4 &&
-        emailCadastro.value.includes("@") &&
-        emailCadastro.value.includes(".") && 
-        emailCadastro.value.includes(".com") &&
-        emailCadastro.checkValidity()
-    )
-    {
-        emailLabel.innerHTML = "E-mail válido. "
-        emailLabel.setAttribute("style", "color: green")
-        emailCadastro.setAttribute('style', 'border-color: green')
+async function cadastrar(cadastro) {
+    try {
+        const requisicao = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify(cadastro)
+        });
         
-    }else {
-        emailLabel.setAttribute("style", "color: red")
-        emailLabel.innerHTML = "E-mail *Insira um e-mail válido"
-        emailCadastro.setAttribute('style', 'border-color: red')
+        const resposta = requisicao.json();
+
+        return resposta;
     }
-
-})
- 
-function fazPost (url, body) {
-    console.log("Body=", body)
-
-    let request = new XMLHttpRequest()
-    request.open("POST", url, true)
-    request.setRequestHeader("Content-type", "application/json")
-    request.setRequestHeader("Access-Control-Allow-Origin", "*")
-
-    request.send(JSON.stringify(body))
-
-    request.onload = function () {
-        console.log(this.responseText)
+    catch {
+        alert('Erro: Tivemos problema com a comunicação com o servidor. Tente novamente em alguns minutos.');
+        throw 'Servidor fora do ar!';
     }
-
-    return request.responseText
 }
 
-function cadastrarUsuario() {
-    event.preventDefault()
-    let url = "http://localhost:8080/usuarios"
+function redirecionarPara(URLdeDestino) {
+    window.location.replace(URLdeDestino);
+}
 
+cadastroForm.addEventListener('submit', (evento) => {
+   
+    evento.preventDefault();
+
+    const inputs = evento.target.elements;
     
-    console.log(url)
+    
+    const nomeCompleto = inputs.nomeCompleto.value;
+    const email = inputs.email.value;
+    
+    const cadastro = { nomeCompleto, email }
 
-    let nome = nomeCadastro.value
-    let email = emailCadastro.value
-
-    console.log(nome)
-    console.log(email)
-
-    body = {
-        "nome": nome,
-        "email": email
-    }
-
-    fazPost(url, body)
-
-    Window.location.replace('http://127.0.0.1:5503/login.html')  
-}
+    cadastrar(cadastro)
+        .then(() => {
+            redirecionarPara('/login');
+        })
+        .catch(error => {
+            console.log(error);
+            alert(`Problemas de comunicação com o servidor. Tente novamente em alguns minutos.`);
+        });
+});
