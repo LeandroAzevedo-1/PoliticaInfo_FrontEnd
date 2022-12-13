89acf0988fc7134084ab896c61a64024a549895c
perkles <leoazevedo1450@gmail.com>
2022-12-12 23:56:13 -0300

    style Cards Despesas

--- assets/js/deputados.js
+++ assets/js/deputados.js
@@ -1,84 +1,88 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor:'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const teste = document.querySelector(".slide-content");
+const inputBusca = document.querySelector("#input-busca");
+const btnBuscar = document.querySelector(".btn-buscar");
+const msgSucess = document.querySelector("#Sucess");
+const deputadoBuscado = document.querySelector(".DeputadoBuscado");
+
+const urlLink = `https://dadosabertos.camara.leg.br/api/v2/deputados`;
+
+const BuscarDeputado = async () => {
+  let url = await fetch(urlLink);
+  let jsonLink = await url.json();
+  let jsonLinkDados = jsonLink.dados;
+
+  let nomeDeputados = jsonLinkDados.map(({ nome }) => {
+    return nome;
+  });
+  return nomeDeputados;
+};
+
+const CardsBusca = async () => {
+  let urlCard = await fetch(urlLink);
+  let jsonLinkCard = await urlCard.json();
+  let jsonLinkCardDados = jsonLinkCard.dados;
+
+  let FirstLetra = inputBusca.value.charAt(0);
+  let returFilter = jsonLinkCardDados.filter((item) => {
+    if (item.nome.charAt(0) == FirstLetra) {
+      return item;
+    }
   });
 
-  const teste = document.querySelector(".slide-content");
-  const inputBusca = document.querySelector("#input-busca")
-  const btnBuscar = document.querySelector(".btn-buscar")
-  const msgSucess = document.querySelector("#Sucess")
-  const deputadoBuscado = document.querySelector(".DeputadoBuscado")
+  let createCard = returFilter.filter((item) => {
+    if (inputBusca.value == item.nome) {
+      return item;
+    }
+  });
 
-  const urlLink = `https://dadosabertos.camara.leg.br/api/v2/deputados`
+  createCard.map(async ({ uri }) => {
+    let uridep = uri;
+    let uriDeputadosUri = await fetch(uridep);
+    let jsonUri = await uriDeputadosUri.json();
+    let jsonUriDados = jsonUri.dados;
 
-  const BuscarDeputado = async () => {
-    let url = await fetch(urlLink)
-    let jsonLink = await url.json()
-    let jsonLinkDados = jsonLink.dados
-  
-    let nomeDeputados = jsonLinkDados.map(({nome}) =>{
-      return nome
-    })
-    return nomeDeputados
-    
-  }
+    if (inputBusca.value === jsonUriDados.ultimoStatus.nome) {
+      const {
+        id,
+        ultimoStatus,
+        nomeCivil,
+        dataNascimento,
+        municipioNascimento,
+        ufNascimento,
+        escolaridade,
+      } = jsonUriDados;
 
-  const CardsBusca = async () => {
-    let urlCard = await fetch(urlLink)
-    let jsonLinkCard = await urlCard.json()
-    let jsonLinkCardDados = jsonLinkCard.dados
-    
-    let FirstLetra = inputBusca.value.charAt(0)
-    let returFilter = jsonLinkCardDados.filter(item => {
-      if(item.nome.charAt(0) == FirstLetra){
-        return item
-      }
-    })
-  
-    let createCard = returFilter.filter(item => {
-      if(inputBusca.value == item.nome){
-        return item 
-      }
-    
-    })
-  
-    createCard.map(async ({uri}) => {
-      let uridep = uri
-      let uriDeputadosUri = await fetch(uridep)
-      let jsonUri = await uriDeputadosUri.json()
-      let jsonUriDados = jsonUri.dados
-
-  
-      if(inputBusca.value === jsonUriDados.ultimoStatus.nome){
-  
-        const {id, ultimoStatus, nomeCivil, dataNascimento, municipioNascimento, ufNascimento, escolaridade} = jsonUriDados
-  
-        deputadoBuscado.innerHTML = `
+      deputadoBuscado.innerHTML = `
         
         <div class="container-deputadoBusado">
   
@@ -95,7 +99,7 @@ var swiper = new Swiper(".slide-content", {
                   <span>Situação: Em ${ultimoStatus.situacao}</span>
                 </div>
   
-                  <div>
+                  <div class="dadosPessoais">
                     <p>Data de Nascimento: ${dataNascimento}</p>
                     <p>Original da cidade ${municipioNascimento} | Estado: ${ufNascimento}</p>
                     <p>Escolaridade: ${escolaridade}</p>
@@ -103,66 +107,86 @@ var swiper = new Swiper(".slide-content", {
                   </div>
             </div>
         </div>
-        `
-  
-        let uriDespesasDeputado = await fetch(`https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/despesas?ano=2022&mes=11&ordem=ASC`)
-        let jsonUri = await uriDespesasDeputado.json();
-        let jsonUriDadosDespesa = jsonUri.dados;
-  
-        jsonUriDadosDespesa.map(({tipoDocumento, nomeFornecedor, dataDocumento, valorDocumento, valorLiquido, urlDocumento}) => {
+
+        <div>
+              <h3 class="TextDespesas">Despesas do mês Atual</h3>
+        </div>
+        `;
+
+      let uriDespesasDeputado = await fetch(
+        `https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/despesas?ano=2022&mes=11&ordem=ASC`
+      );
+      let jsonUri = await uriDespesasDeputado.json();
+      let jsonUriDadosDespesa = jsonUri.dados;
+
+      jsonUriDadosDespesa.map(
+        ({
+          tipoDocumento,
+          nomeFornecedor,
+          dataDocumento,
+          valorDocumento,
+          valorLiquido,
+          urlDocumento,
+        }) => {
           deputadoBuscado.innerHTML += `
-            <div>
-              <h1>${tipoDocumento}</h1>
-              <h2>${valorDocumento} - <em>Valor líquido ${valorLiquido}</em></h2>
-              <p>Nome do forncecedor: ${nomeFornecedor}</p>
-              <p>${dataDocumento}</p>
-  
-              <a href="${urlDocumento}">Link da despesa</a>
+          <section class="sectionDespesasCards">
+            <div class="cardsDespesas">
+              <h1 class="tipoDocumento">${tipoDocumento}</h1>
+
+              <div class="infoCard">
+                <h2 class="valores">${valorDocumento} - <em>Valor líquido ${valorLiquido}</em></h2>
+                <p class="fornecedor">Nome do Forncecedor: ${nomeFornecedor}</p>
+                <p>Data: ${dataDocumento}</p>
+
+                <a class="linkDocumento" target="_blank" href="${urlDocumento}">Link da despesa</a>
+              </div>
+
             </div>
-          `
-        })
-    
-      }
-    }) 
-    
-  }
-  
-  btnBuscar.addEventListener("click", async ()=> {
-    let bntBusca = await BuscarDeputado()
-       
-      if(bntBusca.includes(inputBusca.value)){
-        msgSucess.innerHTML = `Buscado Deputado...`
-        msgSucess.setAttribute("style", "color: green")
-  
-        setTimeout(() => {
-          msgSucess.innerHTML = ""
-          inputBusca.value = ""
-        }, 3000); 
-        inputBusca.focus()
-      }else {
-        msgSucess.innerHTML = `Deputado Não Existe`
-        msgSucess.setAttribute("style", "color: red")
-        deputadoBuscado.innerHTML = ''
-        inputBusca.focus()
-      }
-      CardsBusca()
-  })
-  function deputados() {
-    fetch(urlLink)
-      .then(async (resultDeputados) => {
-        if (!resultDeputados.ok) {
-          throw new Error(resultDeputados.status);
+          </section>
+            
+          `;
         }
-  
-        var deputadoInfo = await resultDeputados.json();
-        var deputadosDados = deputadoInfo.dados
-  
-        deputadosDados.map(({urlFoto, nome, siglaPartido, siglaUf, email}, indice) => {
+      );
+    }
+  });
+};
+
+btnBuscar.addEventListener("click", async () => {
+  let bntBusca = await BuscarDeputado();
+
+  if (bntBusca.includes(inputBusca.value)) {
+    msgSucess.innerHTML = `Buscado Deputado...`;
+    msgSucess.setAttribute("style", "color: green");
+
+    setTimeout(() => {
+      msgSucess.innerHTML = "";
+      inputBusca.value = "";
+    }, 3000);
+    inputBusca.focus();
+  } else {
+    msgSucess.innerHTML = `Deputado Não Existe`;
+    msgSucess.setAttribute("style", "color: red");
+    deputadoBuscado.innerHTML = "";
+    inputBusca.focus();
+  }
+  CardsBusca();
+});
+function deputados() {
+  fetch(urlLink)
+    .then(async (resultDeputados) => {
+      if (!resultDeputados.ok) {
+        throw new Error(resultDeputados.status);
+      }
+
+      var deputadoInfo = await resultDeputados.json();
+      var deputadosDados = deputadoInfo.dados;
+
+      deputadosDados.map(
+        ({ urlFoto, nome, siglaPartido, siglaUf, email }, indice) => {
           let divCol = document.querySelector(".card-wrapper");
-  
-          if(indice < 50) {
-            divCol.innerHTML += 
-            `
+
+          if (indice < 50) {
+            divCol.innerHTML += `
                 <div class="card swiper-slide">
                   <div class="image-content">
                       <span class="overlay"></span>
@@ -194,7 +218,9 @@ var swiper = new Swiper(".slide-content", {
                 `;
           }
           teste.appendChild(divCol);
-        });
-      }).catch((e) => console.log(e));
-  }
-  deputados(); 
+        }
+      );
+    })
+    .catch((e) => console.log(e));
+}
+deputados();
